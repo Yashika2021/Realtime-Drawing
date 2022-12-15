96e416a3a3fa6b392a555a1d7c18550b457f2235
Yashika2021 <88836028+Yashika2021@users.noreply.github.com>
2022-12-15 19:26:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550,420);
+    canvas.position(580, 170);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+    background("#969A97");
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+function gotPoses(results){
+    if(results.length > 0 ){
+        console.log(results);
+    }
+}
