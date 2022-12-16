16aa30bf0c2da7d38c9a78b6da3d58f9886feee0
Yashika2021 <88836028+Yashika2021@users.noreply.github.com>
2022-12-16 19:37:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
-    video.size(550, 500);
-    canvas = createCanvas(550,420);
-    canvas.position(580, 170);
+    video.size(550, 420);
+    video.position(50, 100);
+    canvas = createCanvas(600,420);
+    canvas.position(630, 100);
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
 function draw(){
     background("#969A97");
+    fill("#BFA78C");
+    stroke("#bfbb8c");
+    square(noseX, noseY, difference);
 }
 function modelLoaded(){
     console.log("Model Loaded");
@@ -16,5 +26,12 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0 ){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose X = " +noseX + "nose Y = " +noseY);
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristY = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("right wrist x = " +rightWristX + "left wrist x = " + leftWristX + "difference = " +difference); 
     }
 }
